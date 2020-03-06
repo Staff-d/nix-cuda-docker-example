ff0126192239613b3bd4ad3b25919b5ca13315a4
Sebastian Staffa <Sebastian_Staffa@web.de>
2020-03-06 17:32:50 +0100

    first generic builder

---
+++ buildCudaImage.nix
@@ -0,0 +1,35 @@
+{
+    # see https://github.com/NixOS/nixpkgs/blob/0a351c3f65136c00d3512dd77f48e12a571cf495/pkgs/build-support/docker/default.nix#L656
+    name,
+    cudatoolkit,
+    buildImage,
+    tag ? null,
+    fromImage ? null,
+    fromImageName ? null,
+    fromImageTag ? null,
+    contents ? null,
+    keepContentsDirlinks ? false,
+    config ? {Env = [];},
+    extraCommands ? "", uid ? 0, gid ? 0,
+    runAsRoot ? null,
+    diskSize ? 1024,
+    created ? "1970-01-01T00:00:01Z"
+}:
+
+let
+  cudaVersionString = "CUDA_VERSION=" + cudatoolkit.version;
+
+  cudaEnv = [
+    "${cudaVersionString}"
+    "NVIDIA_VISIBLE_DEVICES=all"
+    "NVIDIA_DRIVER_CAPABILITIES=all"
+
+    "LD_LIBRARY_PATH=/usr/lib64/"
+  ];
+
+  cudaConfig = config // {Env = cudaEnv;};
+
+in buildImage {
+  inherit name tag fromImage fromImageName fromImageTag contents keepContentsDirlinks extraCommands runAsRoot diskSize created;
+  config = cudaConfig;
+}
--- test_image.nix
+++ test_image.nix
@@ -5,22 +5,16 @@
 let
   program = with pkgs;import ./program.nix{inherit stdenv cudatoolkit;};
 
-  cuda_version_string = "CUDA_VERSION=" + cudatoolkit.version;
+in import ./buildCudaImage.nix {
 
-in pkgs.dockerTools.buildImage {
+  buildImage = pkgs.dockerTools.buildImage;
+  cudatoolkit = cudatoolkit;
+
   name = "awesome";
   tag = "latest";
 
   contents = [program pkgs.bash pkgs.coreutils];
   config = {
-    Env= [
-      "${cuda_version_string}"
-      "NVIDIA_VISIBLE_DEVICES=all"
-      "NVIDIA_DRIVER_CAPABILITIES=all"
-
-      "LD_LIBRARY_PATH=/usr/lib64/"
-    ];
     Entrypoint = "/bin/main";
-
   };
 }
